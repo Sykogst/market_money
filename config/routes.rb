8da9467886b3e00b7e2b94433e36cbed9fccb9f0
Sam Tran <samuelttran@gmail.com>
2023-12-05 12:25:18 -0700

    add routes, temp

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
     namespace :api do
       namespace :v0 do
         # get '/markets', to: 'markets#index'
-        resources :markets, only: [:index, :show]
+        resources :markets, only: [:index, :show] do
+          # resources :vendors, only: [:index], controller: 'market_vendors'
+        end
       end
     end
   end
